2527ea7d8151f298461b126545b6bf62fff8dd93
tiemonl <tiemon.liam@gmail.com>
2019-05-23 13:12:35 -0400

    GH-2 added giveaway with random draw feature

--- PitStopBot/PitStopBot.cs
+++ PitStopBot/PitStopBot.cs
@@ -7,71 +7,70 @@ using Discord.Commands;
 using Discord.WebSocket;
 
 namespace PitStopBot {
-	public class PitStopBot {
-		private readonly DiscordSocketClient client;
-		public static string CommandPrefix = "-";
-		private readonly CommandService commands;
+    public class PitStopBot {
+        private readonly DiscordSocketClient client;
+        public static string CommandPrefix = "-";
+        private readonly CommandService commands;
 
-		public PitStopBot() {
-			client = new DiscordSocketClient(new DiscordSocketConfig {
-				LogLevel = LogSeverity.Info
-			});
-			commands = new CommandService();
-			client.MessageReceived += HandleCommandAsync;
-			client.Log += Log;
+        public PitStopBot() {
+            client = new DiscordSocketClient(new DiscordSocketConfig {
+                LogLevel = LogSeverity.Info
+            });
+            commands = new CommandService();
+            client.MessageReceived += HandleCommandAsync;
+            client.Log += Log;
 
-		}
+        }
 
-		public async Task MainAsync() {
-			Console.WriteLine("Which bot to run: ");
-			string key = Console.ReadLine();
-			if (key.Equals("debug")) {
-				CommandPrefix = "?";
-			}
-			string token = GetKey.Get(key).Trim();
+        public async Task MainAsync() {
+            Console.WriteLine("Which bot to run: ");
+            string key = Console.ReadLine();
+            if (key.Equals("debug")) {
+                CommandPrefix = "?";
+            }
+            string token = GetKey.Get(key).Trim();
 
-			await commands.AddModulesAsync(Assembly.GetEntryAssembly(), null);
-			await client.LoginAsync(TokenType.Bot, token, false);
-			await client.StartAsync();
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly(), null);
+            await client.LoginAsync(TokenType.Bot, token, false);
+            await client.StartAsync();
 
-			// Block this task until the program is closed.
-			await Task.Delay(-1);
-		}
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
 
-		private Task Log(LogMessage msg) {
-			Console.WriteLine(msg.ToString());
-			return Task.CompletedTask;
-		}
+        private Task Log(LogMessage msg) {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
 
-		private async Task HandleCommandAsync(SocketMessage arg) {
-			// Bail out if it's a System Message.
-			var msg = arg as SocketUserMessage;
-			if (msg == null) return;
+        private async Task HandleCommandAsync(SocketMessage arg) {
+            // Bail out if it's a System Message.
+            if (!(arg is SocketUserMessage msg)) return;
 
-			// We don't want the bot to respond to itself or other bots.
-			if (msg.Author.Id == client.CurrentUser.Id || msg.Author.IsBot) return;
+            // We don't want the bot to respond to itself or other bots.
+            if (msg.Author.Id == client.CurrentUser.Id || msg.Author.IsBot) return;
 
-			// Create a number to track where the prefix ends and the command begins
-			int pos = 0;
-			// Replace the '!' with whatever character
-			// you want to prefix your commands with.
-			// Uncomment the second half if you also want
-			// commands to be invoked by mentioning the bot instead.
-			if (msg.HasStringPrefix(CommandPrefix, ref pos) /* || msg.HasMentionPrefix(_client.CurrentUser, ref pos) */) {
-				// Create a Command Context.
-				var context = new SocketCommandContext(client, msg);
+            // Create a number to track where the prefix ends and the command begins
+            int pos = 0;
+            // Replace the '!' with whatever character
+            // you want to prefix your commands with.
+            // Uncomment the second half if you also want
+            // commands to be invoked by mentioning the bot instead.
+            if (msg.HasStringPrefix(CommandPrefix, ref pos) /* || msg.HasMentionPrefix(_client.CurrentUser, ref pos) */) {
+                // Create a Command Context.
+                var context = new SocketCommandContext(client, msg);
 
-				// Execute the command. (result does not indicate a return value, 
-				// rather an object stating if the command executed successfully).
-				var result = await commands.ExecuteAsync(context, pos, null);
+                // Execute the command. (result does not indicate a return value, 
+                // rather an object stating if the command executed successfully).
+                var result = await commands.ExecuteAsync(context, pos, null);
 
-				// Uncomment the following lines if you want the bot
-				// to send a message if it failed.
-				// This does not catch errors from commands with 'RunMode.Async',
-				// subscribe a handler for '_commands.CommandExecuted' to see those.
-				if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
-				    await msg.Channel.SendMessageAsync(result.ErrorReason);
-			}
-		}
-	}
+                // Uncomment the following lines if you want the bot
+                // to send a message if it failed.
+                // This does not catch errors from commands with 'RunMode.Async',
+                // subscribe a handler for '_commands.CommandExecuted' to see those.
+                if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+                    await msg.Channel.SendMessageAsync(result.ErrorReason);
+            }
+        }
+    }
 }
--- PitStopBot/Utils/GiveawayUtils.cs
+++ PitStopBot/Utils/GiveawayUtils.cs
@@ -1,10 +1,80 @@
 ﻿using System;
-namespace PitStopBot.Utils
-{
-    public class GiveawayUtils
-    {
-        public GiveawayUtils()
-        {
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace PitStopBot.Utils {
+    public class GiveawayUtils {
+        private EmbedBuilder MyEmbedBuilder = new EmbedBuilder();
+        private EmbedFieldBuilder MyEmbedField = new EmbedFieldBuilder();
+        private readonly Emoji dice = new Emoji("🎲");
+        private readonly Emoji trophy = new Emoji("🏆");
+        private Random rand = new Random();
+        public GiveawayUtils() {
+        }
+        public async Task RunGiveaway(int seconds, string prize, ICommandContext context) {
+
+            //Starts the Embeded Message
+            MyEmbedBuilder.WithColor(Color.DarkBlue);
+            var Name = MyEmbedField.WithName(":game_die: **GIVEAWAY**  :game_die:");
+            MyEmbedField.WithIsInline(true);
+
+            var msg = MyEmbedField.WithValue($"Prize: ***{prize}***\nReact with {dice} to win!\nTime remaining: {seconds} seconds");
+
+
+            //Sends message
+            MyEmbedBuilder.AddField(MyEmbedField);
+            var message = await context.Channel.SendMessageAsync("", false, MyEmbedBuilder.Build());
+
+            //Reacts to message
+            await message.AddReactionAsync(dice);
+
+            //Begins countdown and edits embeded field every hour, minute, or second
+            while (seconds > 0) {
+                await Task.Delay(1000);
+                Console.WriteLine(seconds);
+                seconds--;
+                var newMessage = await message.Channel.GetMessageAsync(message.Id) as IUserMessage;
+                var embed2 = new EmbedBuilder();
+                embed2.AddField(Name);
+                embed2.WithColor(Color.DarkBlue);
+
+                MyEmbedField.WithValue($"Free ***{prize}***\nReact with {dice} to win!\nTime remaining: {seconds} seconds");
+
+                await newMessage.ModifyAsync(m => m.Embed = embed2.Build());
+
+            }
+
+
+            //Adds users to list and randomly selects winner
+            await message.RemoveReactionAsync(dice, message.Author);
+            var temp = await message.GetReactionUsersAsync(dice, 500).FlattenAsync();
+
+            if (temp.Any()) {
+                IUser winner = temp.ElementAt(rand.Next(temp.Count()));
+                var message3 = await message.Channel.GetMessageAsync(message.Id) as IUserMessage;
+                var embed3 = new EmbedBuilder();
+                embed3.AddField(Name);
+                embed3.WithColor(new Color(255, 255, 0));
+
+                MyEmbedField.WithValue($"***Congratulations***! {winner.Mention} You won ***{prize}***!");
+                await message3.ModifyAsync(m => m.Embed = embed3.Build());
+                await message3.AddReactionAsync(trophy);
+            } else {
+                var message4 = await message.Channel.GetMessageAsync(message.Id) as IUserMessage;
+                var embed4 = new EmbedBuilder();
+                embed4.AddField(Name);
+                embed4.WithColor(new Color(255, 255, 0));
+
+                MyEmbedField.WithValue("There are no winners today");
+                await message4.ModifyAsync(m => m.Embed = embed4.Build());
+                await message4.AddReactionAsync(trophy);
+
+            }
+
         }
     }
 }
