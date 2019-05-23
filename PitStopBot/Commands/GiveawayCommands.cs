14399907d3d2c5e99c6f7564433c4aa6400589bf
tiemonl <tiemon.liam@gmail.com>
2019-05-22 21:44:29 -0400

    added default config for live bot

---
+++ PitStopBot/Commands/GiveawayCommands.cs
@@ -0,0 +1,7 @@
+﻿using System;
+namespace PitStopBot.Commands {
+	public class Giveaway {
+		public Giveaway() {
+		}
+	}
+}
--- PitStopBot/GetKey.cs
+++ PitStopBot/GetKey.cs
@@ -1,7 +1,25 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+
 namespace PitStopBot {
-	public class GetKey {
-		public GetKey() {
+	public static class GetKey {
+		public static string Get(string bot) {
+			string keyPath = null;
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+				keyPath = Path.Combine("..","..", "..", "..", "Tokens", $"{bot}.token");
+			else {
+				keyPath = Path.Combine("Tokens", $"{bot}.token");
+			}
+			if (File.Exists(keyPath)) {
+				using (StreamReader sr = new StreamReader(keyPath, Encoding.UTF8)) {
+					string key = sr.ReadToEnd();
+					return key;
+				}
+			}
+			return null;
 		}
 	}
 }
