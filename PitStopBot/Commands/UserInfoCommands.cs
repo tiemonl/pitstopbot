aa79976e414f1d846091e73a90f17eb4d5b8f0b9
tiemonl <tiemon.liam@gmail.com>
2019-05-25 16:44:40 -0400

    GH-21 cleaned up count and embedbuilder code

--- PitStopBot/Commands/UserInfoCommands.cs
+++ PitStopBot/Commands/UserInfoCommands.cs
@@ -4,6 +4,8 @@ using Discord.Commands;
 using PitStopBot.Objects;
 using PitStopBot.Utils;
 using Nethereum.Util;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PitStopBot.Commands {
 	[Group("user")]
@@ -31,32 +33,22 @@ namespace PitStopBot.Commands {
 		[Command("rarities"), Summary("returns the parts count")]
 		public async Task GetRarities([Summary("User's eth adress")] string addressInput) {
 			var address = await GetFormattedAddress(addressInput);
-			int common = 0, rare = 0, epic = 0, legendary = 0;
 			Inventory inv = await userUtils.GetInventory(address);
 			var parts = inv.parts;
+			var rarityList = new List<string>();
 			foreach (var p in parts) {
-				var rarity = p.details.rarity;
-				switch (rarity) {
-					case "Common":
-						common++;
-						break;
-					case "Rare":
-						rare++;
-						break;
-					case "Epic":
-						epic++;
-						break;
-					case "Legendary":
-						legendary++;
-						break;
-				}
+				rarityList.Add(p.details.rarity);
 			}
 			MyEmbedBuilder.WithTitle("Part Rarity Distribution");
 			MyEmbedBuilder.WithColor(Color.Blue);
-			MyEmbedBuilder.AddField("Common", common, true);
-			MyEmbedBuilder.AddField("Rare", rare, true);
-			MyEmbedBuilder.AddField("Epic", epic, true);
-			MyEmbedBuilder.AddField("Legendary", legendary, true);
+			var order = "CREL"; //common, rare, epic, legendary
+			var orderDict = order.Select((c, i) => new { Letter = c, Order = i })
+								 .ToDictionary(o => o.Letter, o => o.Order);
+			var rarities = rarityList.OrderBy(i =>orderDict[i[0]]).GroupBy(i => i);
+			foreach (var r in rarities) {
+				MyEmbedBuilder.AddField(r.Key, r.Count(), true);
+			}
+
 			await ReplyAsync(embed: MyEmbedBuilder.Build());
 		}
 
@@ -94,55 +86,29 @@ namespace PitStopBot.Commands {
 		[Command("elites"), Summary("returns the parts count")]
 		public async Task GetEliteCount([Summary("User's eth adress")] string addressInput) {
 			var address = await GetFormattedAddress(addressInput);
-			int elite = 0;
 			Inventory inv = await userUtils.GetInventory(address);
-			var parts = inv.parts;
-			foreach (var p in parts) {
-				if (p.details.isElite) elite++;
-			}
+
 			MyEmbedBuilder.WithTitle("Elite Parts:");
 			MyEmbedBuilder.WithColor(Color.LightGrey);
-			MyEmbedBuilder.AddField("Count", elite, true);
+			MyEmbedBuilder.AddField("Count", inv.parts.Count(i => i.details.isElite), true);
 			await ReplyAsync(embed: MyEmbedBuilder.Build());
 		}
 
 		[Command("brands"), Summary("returns the brand count")]
 		public async Task GetBrands([Summary("User's eth adress")] string addressInput) {
 			var address = await GetFormattedAddress(addressInput);
-			int bolt = 0, guerilla = 0, hyperion = 0, python = 0, vista = 0, zeta = 0;
 			Inventory inv = await userUtils.GetInventory(address);
 			var parts = inv.parts;
+			var brandList = new List<string>();
 			foreach (var p in parts) {
-				var type = p.details.brand;
-				switch (type) {
-					case "Bolt":
-						bolt++;
-						break;
-					case "Guerilla":
-						guerilla++;
-						break;
-					case "Hyperion":
-						hyperion++;
-						break;
-					case "Python":
-						python++;
-						break;
-					case "Vista":
-						vista++;
-						break;
-					case "Zeta":
-						zeta++;
-						break;
-				}
+				brandList.Add(p.details.brand);
 			}
 			MyEmbedBuilder.WithTitle("Brand Distribution");
 			MyEmbedBuilder.WithColor(Color.DarkMagenta);
-			MyEmbedBuilder.AddField("Bolt", bolt, true);
-			MyEmbedBuilder.AddField("Guerilla", guerilla, true);
-			MyEmbedBuilder.AddField("Hyperion", hyperion, true);
-			MyEmbedBuilder.AddField("Python", python, true);
-			MyEmbedBuilder.AddField("Vista", vista, true);
-			MyEmbedBuilder.AddField("Zeta", zeta, true);
+			var brands = brandList.GroupBy(i => i).OrderBy(i => i.Key);
+			foreach(var b in brands) {
+				MyEmbedBuilder.AddField(b.Key, b.Count(), true);
+			}
 			await ReplyAsync(embed: MyEmbedBuilder.Build());
 		}
 	}
