efbeedf699b9467f7e3f6054222dd26c93f07ed7
tiemonl <tiemon.liam@gmail.com>
2019-05-24 22:40:00 -0400

    GH-15 added the ability to view part info

--- PitStopBot/Objects/Inventory.cs
+++ PitStopBot/Objects/Inventory.cs
@@ -1,30 +1,6 @@
 ﻿using System.Collections.Generic;
 
 namespace PitStopBot.Objects {
-	public class Details {
-		public string internalId { get; set; }
-		public bool isElite { get; set; }
-		public string serialNumber { get; set; }
-		public int durability { get; set; }
-		public int weight { get; set; }
-		public int steering { get; set; }
-		public string model { get; set; }
-		public int power { get; set; }
-		public string type { get; set; }
-		public string brand { get; set; }
-		public int speed { get; set; }
-		public string rarity { get; set; }
-	}
-
-	public class Part {
-		public string external_url { get; set; }
-		public Details details { get; set; }
-		public string description { get; set; }
-		public string id { get; set; }
-		public string name { get; set; }
-		public string image { get; set; }
-	}
-
 	public class Inventory {
 		public List<Part> parts { get; set; }
 		public string total { get; set; }
