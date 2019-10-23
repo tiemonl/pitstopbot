0f89b6fdad9cac63c761dcfe846bd747efa906e2
tiemonl <tiemon.liam@gmail.com>
2019-10-23 18:51:43 -0400

    GH-42: refactored type naming

---
+++ PitStopBot/Utils/StringUtils.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace PitStopBot.Utils {
+    public class StringUtils {
+        public StringUtils() {
+        }
+
+        public static string RenameType(string type) {
+            return type == "wheels" ? "Wheels" : type == "casing" ? "Body" : type == "spoiler" ? "Rear" : "Front";
+        }
+    }
+}
