79fce84f87161cd40d222354dcea98137d6c09ed
tiemonl <tiemon.liam@gmail.com>
2019-10-23 20:25:26 -0400

    GH-44: Changed back to previous API for ENS

---
+++ PitStopBot/Objects/ENS.cs
@@ -0,0 +1,10 @@
+﻿namespace PitStopBot.Objects {
+    public class ENS {
+        public string address {
+            get; set;
+        }
+        public string domain {
+            get; set;
+        }
+    }
+}
